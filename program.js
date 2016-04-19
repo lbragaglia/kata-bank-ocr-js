f4d6bd1fc00d7e75bb226111165e01301ea4a99d
Lorenzo Bragaglia <lbragaglia@users.noreply.github.com>
2016-04-20 00:41:24 +0200

    Fix parsing

--- program.js
+++ program.js
@@ -1,33 +1,43 @@
-var _0 = ' _ ' +
-'| |' +
-'|_|' +
-'   ';
-var patterns = {
-  _0: '0'
-};
+var _0 =
+  ' _ ' +
+  '| |' +
+  '|_|' +
+  '   ';
+var _1 =
+    '   ' +
+    '  |' +
+    '  |' +
+    '   ';
+var patterns = {};
+patterns[_0] = '0';
+patterns[_1] = '1';
 module.exports = {
   parse: function(text) {
     var lines = text.split('\n');
-    
+
     var digits = [];
     for (var l = 0; l < lines.length; l += 4) {
-      console.log(lines[l])
+      if (!lines[l].length) {
+        continue;
+      }
+
       if (lines[l].length != 27) {
-        throw new Error('Numero di caratteri errato: ' + lines[l].length);  
+        throw new Error('Numero di caratteri errato: ' + lines[l].length);
       }
       // saltare ultime righe se rimanenti < 4
-      
+
       digits[l] = '';
       for (var d = 0; d < 27; d += 3) {
-        digits[l] = patterns[ lines[l][d] + lines[l][d+1] + lines[l][d+2] +
-          lines[l+1][d] + lines[l+1][d+1] + lines[l+1][d+2] + 
-          lines[l+2][d] + lines[l+2][d+1] + lines[l+2][d+2]
-        ];
+        var key = lines[l][d] + lines[l][d + 1] + lines[l][d + 2] +
+          lines[l + 1][d] + lines[l + 1][d + 1] + lines[l + 1][d + 2] +
+          lines[l + 2][d] + lines[l + 2][d + 1] + lines[l + 2][d + 2] +
+          lines[l + 3][d] + lines[l + 3][d + 1] + lines[l + 3][d + 2];
+        
+        digits[Math.floor(l/4)] += patterns[key];
       }
-      
     }
-    
-    
-    return digits[0];
+
+
+    return digits.join();
   }
 }
--- test/test.js
+++ test/test.js
@@ -3,12 +3,14 @@ var parser = require('../program.js');
 
 describe('KataBankOCR', function() {
   describe('User Story 1', function() {
-    it('should parse account number', function() {
-      
-      var _0 = [' _ ',
-                '| |',
-                '|_|',
-                '   '];
+    it('should parse account number 000000000', function() {
+
+      var _0 = [
+        ' _ ',
+        '| |',
+        '|_|',
+        '   '
+      ];
       var input = '';
       for (var l = 0; l < 4; l++) {
         for (var i = 0; i < 9; i++) {
@@ -16,9 +18,24 @@ describe('KataBankOCR', function() {
         }
         input += '\n';
       }
-      console.log(input);
       assert.equal('000000000', parser.parse(input));
-      
+    });
+
+    it('should parse account number 111111111', function() {
+      var _1 = [
+        '   ',
+        '  |',
+        '  |',
+        '   '
+      ];
+      var input = '';
+      for (var l = 0; l < 4; l++) {
+        for (var i = 0; i < 9; i++) {
+          input += _1[l];
+        }
+        input += '\n';
+      }
+      assert.equal('111111111', parser.parse(input));
     });
   });
 });
